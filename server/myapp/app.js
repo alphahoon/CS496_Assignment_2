420869848026f25a16e1c88b1bf6037201aa7996
alphahoon <alphahoon@kaist.ac.kr>
2016-12-31 12:02:07 +0000

    contacts db created

--- server/myapp/app.js
+++ server/myapp/app.js
@@ -7,10 +7,9 @@ var bodyParser = require('body-parser');
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+var db = monk('localhost:27017/project2');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -24,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
 app.use(function(req, res, next) {
@@ -32,8 +31,8 @@ app.use(function(req, res, next) {
   next();
 });
 
+// route information
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
