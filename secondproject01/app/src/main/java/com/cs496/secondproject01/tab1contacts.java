88a1d4a255b6217b3a1a51a1998a685e9dc3d2eb
earlgreymacaron <yeaseul.parky@gmail.com>
2016-12-31 17:17:26 +0900

    completed listview

--- secondproject01/app/src/main/java/com/cs496/secondproject01/tab1contacts.java
+++ secondproject01/app/src/main/java/com/cs496/secondproject01/tab1contacts.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -21,10 +22,17 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import static com.cs496.secondproject01.R.id.container;
 
 
 public class tab1contacts extends Fragment {
+    InputStream contacts;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,14 +51,51 @@ public class tab1contacts extends Fragment {
         });
 */
         //if (!isLoggedIn() && App.firstAccess) {
-        if (!isLoggedIn()) {
+        //if (!isLoggedIn()) {
+        if (true) {
             //LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
             //loginButton.setVisibility(View.INVISIBLE);
-            App.firstAccess = false;
+            //App.firstAccess = false;
             Intent popupIntent = new Intent(getActivity(), LoginPop.class);
             startActivity(popupIntent);
         }
+
+        //Facebook sdk에서 friends 받아오기
+        try {
+            contacts = getActivity().getAssets().open("contacts.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray contactsjson = JSONparse(contacts);
+
+        //Handle ListView
+        ListView conList = (ListView) view.findViewById(R.id.contact_list);
+        ContactViewAdapter adapter = new ContactViewAdapter(getActivity(), R.layout.contact_item, contactsjson);
+        conList.setAdapter(adapter);
         return view;
+    }
+
+
+    JSONArray JSONparse (InputStream raw) {
+        String json = null;
+        try {
+            int size = raw.available();
+            byte[] buffer = new byte[size];
+            raw.read(buffer);
+            raw.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            JSONArray contactsjson = new JSONArray(json);
+            return contactsjson;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public void onResume() {
